bfae1e52900c70bdf8788f4cc1f96bdfc276a90d
bakhaa <bakhaa101@gmail.com>
2018-05-08 11:45:38 +0500

    eslint rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,6 +2,8 @@ module.exports = {
     "extends": "airbnb",
     "rules": {
       "react/jsx-filename-extension": 0,
-      "function-paren-newline": 0
+      "function-paren-newline": 0,
+      "jsx-a11y/no-static-element-interactions": 0,
+      "jsx-a11y/click-events-have-key-events": 0
     }
 };
