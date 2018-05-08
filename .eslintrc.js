23906589392fb0fd7918dc80ccf13a226a272d0c
bakhaa <bakhaa101@gmail.com>
2018-05-08 12:23:09 +0500

    eslint rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,6 +4,8 @@ module.exports = {
       "react/jsx-filename-extension": 0,
       "function-paren-newline": 0,
       "jsx-a11y/no-static-element-interactions": 0,
-      "jsx-a11y/click-events-have-key-events": 0
+      "jsx-a11y/click-events-have-key-events": 0,
+      "object-curly-newline": 0,
+      "no-underscore-dangle": 0
     }
 };
